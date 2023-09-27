106a65fc9deca9dc4d29afcd6239d8906cad18ab
joseluiscontreras <joseluis.contreras@innocv.com>
2023-09-27 18:14:23 +0200

    model binding

--- ControllersExample/Controllers/BookstoreController.cs
+++ ControllersExample/Controllers/BookstoreController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ControllersExample.Models;
 
 namespace ControllersExample.Controllers
 {
@@ -6,15 +7,27 @@ namespace ControllersExample.Controllers
     public class BookstoreController : Controller
     {
 
-        [Route("{id}")]
-        public IActionResult Index()
+        [Route("{bookId?}/{isLoggedIn?}")]
+        public IActionResult Index([FromRoute] int? bookId, [FromQuery] bool? isLoggedIn, Book book)
         {
-            int bookId = Convert.ToInt32(Request.RouteValues["id"]);
-            return RedirectToActionPermanent("Books", "Store", new { id = bookId });
+            if (bookId.HasValue == false)
+            {
+                return BadRequest("Book id is not supplied");
+            }
 
-            // return LocalRedirectPermanent($"/store/books/{bookId}");
+            if (bookId <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
 
-            // return RedirectPermanent($"store/books/{bookId}");
+            if (isLoggedIn == false)
+            {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookId}, Book: {book}");
+
+            // return RedirectToActionPermanent("Books", "Store", new { id = bookId });
         }
     }
 }
